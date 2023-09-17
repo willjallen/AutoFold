bc29a79c38465d6d4428de8c1c4edc902465c50e
willjallen <wallen2268@gmail.com>
2023-09-17 16:22:52 -0500

    Add template strategy

---
+++ strategies/template_strategy.py
@@ -0,0 +1,20 @@
+from tinydb import TinyDB, Query
+from strategies.strategy import Strategy
+from bot import Bot
+from manifold_api import ManifoldAPI
+from manifold_database import ManifoldDatabaseReader
+from manifold_subscriber import ManifoldSubscriber
+
+class TemplateStrategy(Strategy):
+    def __init__(self, bot: Bot, manifold_api: ManifoldAPI, manifold_db_reader: ManifoldDatabaseReader, manifold_subscriber: ManifoldSubscriber):
+        super().__init__(name=__name__)
+        self.bot = bot
+        self.manifold_api = manifold_api
+        self.manifold_db_reader = manifold_db_reader
+        self.manifold_subscriber = manifold_subscriber
+        
+    def run(self):
+        pass
+    
+    def shutdown(self):
+        pass
